5060520cbf7761e146c596efc2cf02f4648ca314
BALAJI <kbalajireddy111@gmail.com>
2024-02-03 15:48:10 +0530

    catalogue

--- catalogue-deploy/terraform/main.tf
+++ catalogue-deploy/terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "catalogue" {
   name                 = "${local.name}-${var.tags.component}"
-  port                 = 80
+  port                 = 8080
   protocol             = "HTTP"
   vpc_id               = data.aws_ssm_parameter.vpc_id.value
   deregistration_delay = 60
@@ -10,7 +10,7 @@ resource "aws_lb_target_group" "catalogue" {
     unhealthy_threshold = 3
     timeout             = 5
     path                = "/health"
-    port                = 80
+    port                = 8080
     matcher             = "200-299"
   }
 }
